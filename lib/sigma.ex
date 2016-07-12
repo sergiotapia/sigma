ea0c2cd17cc9ab094ac0d92b890678cfeebf9873
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2016-07-11 23:32:52 -0400

    Enable status.io parsing/

--- lib/sigma.ex
+++ lib/sigma.ex
@@ -3,6 +3,8 @@ defmodule Sigma do
     cond do
       String.contains?(body, "statuspage.io") ->
         :statuspage_io
+      String.contains?(body, "Status.io") ->
+        :status_io
       true ->
         :not_found
     end
@@ -18,6 +20,8 @@ defmodule Sigma do
     case detect_provider(body) do
       :statuspage_io ->
         Sigma.Parsers.StatusPageIO.status(body)
+      :status_io ->
+        Sigma.Parsers.StatusIO.status(body)
       _ ->
         "Unkown"
     end
