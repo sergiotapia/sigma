8bcc70cf7b2dc03f7116626bbb744bcebc4b8fd4
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2016-07-11 23:03:19 -0400

    Remove tests in wrong file.

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -31,14 +31,4 @@ defmodule SigmaParserTest do
     provider = Sigma.Parser.detect_provider(body)
     assert provider == :statuspage_io
   end
-
-  test "lists all available companies we track status for" do
-    companies = Sigma.Companies.list_companies
-    assert Enum.count(companies) == 374
-  end
-
-  test "finds a company's status website url" do
-    url = Sigma.Companies.company_url(:acquia)
-    assert url == "https://status.acquia.com"
-  end
 end
