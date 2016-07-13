0c7a1d522fba96139f3d51aff52a0c3499c0f494
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2016-07-13 07:30:19 -0400

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sigma.Mixfile do
 
   def project do
     [app: :sigma,
-     version: "1.0.4",
+     version: "1.0.5",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
